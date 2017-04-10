746af714ad9757a3b090c83865f8539e5d720fe7
Min K <yegofm@yahoo.com>
2017-04-09 21:53:18 -0400

    Create App.java
    java file

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.afrohitech;
+
+public class App {
+	public static String sample(){
+		return "Sample Text";
+	}
+
+	public static void main(String[] args) {
+		sample();
+		System.out.println("Hello World!!");
+	}
+
+}
